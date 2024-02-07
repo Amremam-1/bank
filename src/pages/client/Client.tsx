e060e5df9492ba112c63f506bf1564d7e32942b1
Amr Imam <emama2337@gmail.com>
2024-02-07 19:39:50 +0200

    page client

--- src/pages/client/Client.tsx
+++ src/pages/client/Client.tsx
@@ -1,7 +1,83 @@
+import * as React from "react"
 import styles from "./styles.module.css"
+import fakeDate from "../../../MOCK_DATA.json"
+import { useTable, Column } from "react-table"
+
+interface UserData {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  gender: string
+  account_balance: string
+}
 
 const Client = () => {
-  return <div className={styles.client}>Client</div>
+  const data = React.useMemo(() => fakeDate, [])
+  const columns: Column<UserData>[] = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Gender",
+        accessor: "gender",
+      },
+      {
+        Header: "Account Balance",
+        accessor: "account_balance",
+      },
+    ],
+    []
+  )
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data })
+
+  return (
+    <div className={styles.parent}>
+      <div className={styles.container}>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default Client
